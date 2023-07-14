0765edbef76b930b2dcfadc13d4b45dfd620b377
samer201001 <samer201001@gmail.com>
2023-07-14 21:03:48 +0200

    pricinglocation

--- backend/app.js
+++ backend/app.js
@@ -74,35 +74,37 @@ app.post('/webhooks', (req, res) => {
   res.json({received: true});
 })
 
-const storeItems = new Map([
-  [1, { priceInCents: 30000, name: "Tier 1"}],
-  [2, { priceInCents: 50000, name: "Tier 2"}],
-])
-
 app.post("/create-checkout-session", async (req, res) => {
   try {
+    const storeItems = new Map([
+      [1, { priceInCents: 30000, name: "Tier 1" }],
+      [2, { priceInCents: 50000, name: "Tier 2" }],
+    ]);
+
+    const lineItems = req.body.items.map((item) => {
+      const storeItem = storeItems.get(item.id);
+      return {
+        price_data: {
+          currency: "usd",
+          recurring: {
+            interval: "month",
+          },
+          product_data: {
+            name: storeItem.name,
+          },
+          unit_amount: storeItem.priceInCents,
+        },
+        quantity: item.quantity,
+      };
+    });
+
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       mode: "subscription",
-      line_items: req.body.items.map((item) => {
-        const storeItem = storeItems.get(item.id);
-        return {
-          price_data: {
-            currency: "usd",
-            recurring: {
-              interval: "month",
-            },
-            product_data: {
-              name: storeItem.name,
-            },
-            unit_amount: storeItem.priceInCents,
-          },
-          quantity: item.quantity,
-        };
-      }),
+      line_items: lineItems,
       success_url: "http://localhost:5173/success",
       cancel_url: "http://localhost:5173/cancel",
-      customer_email: req.body.email || "example@example.com", // Set default email if not provided
+      customer_email: req.body.email || "example@example.com",
     });
 
     res.json({ url: session.url });
